fd9689297fd000dc0eb36610ab22739a4f42f090
wjjhhj0914 <wjjhhj0914@gmail.com>
2025-08-15 14:26:41 +0900

    learn. Review of Essential JavaScript for React : Asynchronous JavaScript: Asynce/Await

--- 02-JS-review/starter/script.js
+++ 02-JS-review/starter/script.js
@@ -259,8 +259,23 @@ const booksAfterUpdate = booksAfterDelete.map(book =>
 booksAfterUpdate;
 */
 
-fetch('https://jsonplaceholder.typicode.com/todos').then(res =>
-  res.json().then(data => console.log(data))
-);
+// fetch('https://jsonplaceholder.typicode.com/todos').then(res =>
+//   res.json().then(data => console.log(data))
+// );
+
+// console.log('jonas');
+
+async function getTodos() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/todos');
+  const data = await res.json();
+  console.log(data);
+
+  return data;
+}
+
+const todos = getTodos();
+console.log(todos);
+
+getTodos();
 
 console.log('jonas');
